3c1640fa0d4128c2270e21224fd6469d7b95bee9
itsjustaplant <alperensahin442@gmail.com>
2020-10-05 10:33:27 +0300

    Usage is added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 
 #include "../include/midi_file.h"
 using namespace std;
-int main() {
-    MidiFile midi_file("../midi_files/teddybear.mid");
-    midi_file.ReadHeaderChunk();
-    midi_file.PrintInfo();
+#define USAGE  \
+    "Usage: midi_parser path\n" \
+    "            path: path of the input file " \
+
+int main(int argc, char* argv[]) {
+    if(argc == 2){
+        MidiFile midi_file(argv[1]);
+        midi_file.ReadHeaderChunk();
+
+        string command;
+        cout << ">> ";
+        cin >> command;
+        while(command != "exit"){
+            if(command == "info"){
+                midi_file.PrintInfo();
+            } else if(command == "usage"){
+                cout << USAGE << endl;
+            } else{
+                cout << "No command" << endl;
+            }
+            cout << ">> ";
+            cin >> command;
+        }
+    } else{
+        cout << USAGE << endl;
+    }
     return 0;
 }
