3b6a7c648de05f874b5641a1be9a3f5c2304b384
itsjustaplant <alperensahin442@gmail.com>
2020-10-08 11:09:50 +0300

    include/Track.h
    Track class is created

---
+++ include/Track.h
@@ -0,0 +1,17 @@
+//
+// Created by Alperen on 7.10.2020.
+//
+
+#ifndef MIDI_PARSER_TRACK_H
+#define MIDI_PARSER_TRACK_H
+
+
+class Track{
+public:
+    int unsigned length;
+    char* data;
+    Track()=default;
+};
+
+
+#endif //MIDI_PARSER_TRACK_H
