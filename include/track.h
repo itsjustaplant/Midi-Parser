69ea7f303e4789bb52442d7aa236c862a05e8e26
itsjustaplant <alperensahin442@gmail.com>
2020-10-11 15:34:12 +0300

    Program can read multiple events for a track now

--- include/track.h
+++ include/track.h
@@ -4,24 +4,39 @@
 
 #ifndef MIDI_PARSER_TRACK_H
 #define MIDI_PARSER_TRACK_H
-#define DELTA_TIME_SIZE     8 // it is fixed to 1 byte for now
+
 #include <iostream>
 #include <sstream>
+
 //<Track Chunk> = <chunk type><length><MTrk event>+
 //
 //The syntax of an MTrk event is very simple:
 //
 //<MTrk event> = <delta-time><event>
+
+/*
+header -> type -> len,01,02,03,04,05 -> len,text,etc
+FF 00 02 Sequence Number
+FF 01 len text Text Event
+FF 02 len text Copyright Notice
+FF 03 len text Sequence/Track Name
+FF 04 len text Instrument Name
+FF 05 len text Lyric
+FF 06 len text Marker
+FF 07 len text Cue Point
+FF 20 01 cc MIDI Channel Prefix
+FF 2F 00 End of Track
+FF 51 03 tttttt Set Tempo (in microseconds per MIDI quarter-note)
+FF 54 05 hr mn se fr ff SMPTE Offset
+FF 58 04 nn dd cc bb Time Signature */
 class Track{
 public:
-    int event_check_[8] = {0,0,0,0,0,0,0,0};
+    int event_check_[7] = {0,0,0,0,0,0,0};
+
     //These variables will be initialized in ReadTrackChunk()
     int unsigned length_;
     char* data_;
-
-    //These variables will be initialized in ReadTrackData()
-    std::string delta_time_;
-    std::string event_;
+    std::string temp_data;
 
     //These variables will be initialized in ClassifyMetaEvent()
     std::string text_event_;
@@ -40,17 +55,24 @@ public:
     void ReadTrackData();
 
     /**
-     * @brief Classify the meta event by checking the 3rd and 4th byte of meta event
-     */
-    void ClassifyMetaEvent();
-
-    /**
      * @brief Takes string of binary array and convert it's to ASCII text
      *        It is used for converting binary copyright, lyric, track name etc. to string
      * @param str
      * @return It returns std::string
      */
     std::string static BinToString(std::string* str);
+    /*------- There lies a spaghetti, pls don't scroll down-------- */
+    /**
+     * @brief Seeks for FF XX if it finds, it initializes this -> XX and checks the event_check[X]
+     *                        else does nothing
+     */
+    void SetText();
+    void SetCopyright();
+    void SetTrackName();
+    void SetInstrumentName();
+    void SetLyric();
+    void SetMarker();
+    void SetCuePoint();
 };
 
 
--- src/track.cpp
+++ src/track.cpp
@@ -14,105 +14,142 @@ std::string Track::BinToString(std::string* str){
     }
     return output;
 }
-void Track::ReadTrackData() {
 
-    for (int i = 0; i < 1; ++i) {
-        this -> delta_time_.append((std::bitset<8>(this -> data_[i]).to_string()));
-    }
-    //Each event is longer than 2 bytes
-    //To classify the even it only reads 2 bytes and checks the second byte
-    for (int i = 2; i < 3; ++i) {
-        this -> event_.append(std::bitset<8>(this -> data_[i]).to_string());
-    }
-    this -> ClassifyMetaEvent();
-}
-/*
- *
-FF 00 02 Sequence Number
-FF 01 len text Text Event
-FF 02 len text Copyright Notice
-FF 03 len text Sequence/Track Name
-FF 04 len text Instrument Name
-FF 05 len text Lyric
-FF 06 len text Marker
-FF 07 len text Cue Point
-FF 20 01 cc MIDI Channel Prefix
-FF 2F 00 End of Track
-FF 51 03 tttttt Set Tempo (in microseconds per MIDI quarter-note)
-FF 54 05 hr mn se fr ff SMPTE Offset
-FF 58 04 nn dd cc bb Time Signature */
-
-void Track::ClassifyMetaEvent() {
-    int meta_event = std::stoi(this -> event_, nullptr, 2);
-    if(meta_event == 0){
-    } else if(meta_event >= 1 && meta_event <= 7){
+void Track::SetText() {
+    size_t found = this -> temp_data.find("1111111100000001");
+    if (found != std::string::npos){
+        int position = (int)found / 8;
         std::string temp_length;
-        int temp_length_i;
-        for (int i = 3; i < 4; ++i) {
+        std::string temp_text;
+        for (int i = position + 2; i < position + 3; ++i) {
             temp_length.append(std::bitset<8>(this -> data_[i]).to_string());
         }
-        temp_length_i = std::stoi(temp_length, nullptr, 2);
-        switch (meta_event) {
-            case 1: {
-                std::string temp_event;
-                for (int i = 4; i < 4 + temp_length_i; ++i) {
-                    temp_event.append(std::bitset<8>(this -> data_[i]).to_string());
-                }
-                this -> text_event_ = BinToString(&temp_event);
-                this -> event_check_[1] = 1;
-                break;
-            }
-            case 2: {
-                std::string temp_copyright;
-                for (int i = 4; i < 4 + temp_length_i; ++i) {
-                    temp_copyright.append(std::bitset<8>(this -> data_[i]).to_string());
-                }
-                this -> copyright_ = BinToString(&temp_copyright);
-                this -> event_check_[2] = 1;
-                break;
-            }
-            case 3: {
-                std::string temp_name;
-                for (int i = 4; i < 4 + temp_length_i; ++i) {
-                    temp_name.append(std::bitset<8>(this->data_[i]).to_string());
-                }
-                this->track_name_ = BinToString(&temp_name);
-                this -> event_check_[3] = 1;
-                break;
-            }
-            case 4:{
-                std::string temp_instrument;
-                for (int i = 4; i < 4 + temp_length_i; ++i) {
-                    temp_instrument.append(std::bitset<8>(this -> data_[i]).to_string());
-                }
-                this -> instrument_name_ = BinToString(&temp_instrument);
-                this -> event_check_[4] = 1;
-            }
-            case 5:{
-                std::string temp_lyric;
-                for (int i = 4; i < 4 + temp_length_i; ++i) {
-                    temp_lyric.append(std::bitset<8>(this -> data_[i]).to_string());
-                }
-                this -> lyric_ = BinToString(&temp_lyric);
-                this -> event_check_[5];
-            }
-            case 6:{
-                std::string temp_marker;
-                for (int i = 4; i < 4 + temp_length_i; ++i) {
-                    temp_marker.append(std::bitset<8>(this -> data_[i]).to_string());
-                }
-                this -> marker_ = BinToString(&temp_marker);
-                this -> event_check_[6] = 1;
-            }
-            case 7:{
-                std::string temp_cue;
-                for (int i = 4; i < 4 + temp_length_i; ++i) {
-                    temp_cue.append(std::bitset<8>(this -> data_[i]).to_string());
-                }
-                this -> cue_point_ = BinToString(&temp_cue);
-                this -> event_check_[7] = 1;
-            }
+        int temp_length_i = std::stoi(temp_length, nullptr, 2);
+        for (int i = position + 3; i < position + 3 + temp_length_i; ++i) {
+            temp_text.append(std::bitset<8>(this -> data_[i]).to_string());
         }
+        this -> text_event_ = BinToString(&temp_text);
+        this -> event_check_[0] = 1;
     }
+}
 
+void Track::SetCopyright() {
+    size_t found = this -> temp_data.find("1111111100000010");
+    if (found != std::string::npos){
+        int position = (int)found / 8;
+        std::string temp_length;
+        std::string temp_text;
+        for (int i = position + 2; i < position + 3; ++i) {
+            temp_length.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        int temp_length_i = std::stoi(temp_length, nullptr, 2);
+        for (int i = position + 3; i < position + 3 + temp_length_i; ++i) {
+            temp_text.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        this -> copyright_ = BinToString(&temp_text);
+        this -> event_check_[1] = 1;
+    }
+}
+
+void Track::SetTrackName() {
+    size_t found = this -> temp_data.find("1111111100000011");
+    if (found != std::string::npos){
+        int position = (int)found / 8;
+        std::string temp_length;
+        std::string temp_text;
+        for (int i = position + 2; i < position + 3; ++i) {
+            temp_length.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        int temp_length_i = std::stoi(temp_length, nullptr, 2);
+        for (int i = position + 3; i < position + 3 + temp_length_i; ++i) {
+            temp_text.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        this -> track_name_ = BinToString(&temp_text);
+        this -> event_check_[2] = 1;
+    }
+}
+
+void Track::SetInstrumentName() {
+    size_t found = this -> temp_data.find("1111111100000100");
+    if (found != std::string::npos){
+        int position = (int)found / 8;
+        std::string temp_length;
+        std::string temp_text;
+        for (int i = position + 2; i < position + 3; ++i) {
+            temp_length.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        int temp_length_i = std::stoi(temp_length, nullptr, 2);
+        for (int i = position + 3; i < position + 3 + temp_length_i; ++i) {
+            temp_text.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        this -> instrument_name_ = BinToString(&temp_text);
+        this -> event_check_[3] = 1;
+    }
+}
+
+void Track::SetLyric() {
+    size_t found = this -> temp_data.find("1111111100000101");
+    if (found != std::string::npos){
+        int position = (int)found / 8;
+        std::string temp_length;
+        std::string temp_text;
+        for (int i = position + 2; i < position + 3; ++i) {
+            temp_length.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        int temp_length_i = std::stoi(temp_length, nullptr, 2);
+        for (int i = position + 3; i < position + 3 + temp_length_i; ++i) {
+            temp_text.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        this -> lyric_ = BinToString(&temp_text);
+        this -> event_check_[4] = 1;
+    }
+}
+
+void Track::SetMarker() {
+    size_t found = this -> temp_data.find("1111111100000110");
+    if (found != std::string::npos){
+        int position = (int)found / 8;
+        std::string temp_length;
+        std::string temp_text;
+        for (int i = position + 2; i < position + 3; ++i) {
+            temp_length.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        int temp_length_i = std::stoi(temp_length, nullptr, 2);
+        for (int i = position + 3; i < position + 3 + temp_length_i; ++i) {
+            temp_text.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        this -> marker_ = BinToString(&temp_text);
+        this -> event_check_[5] = 1;
+    }
+}
+
+void Track::SetCuePoint() {
+    size_t found = this -> temp_data.find("1111111100000111");
+    if (found != std::string::npos){
+        int position = (int)found / 8;
+        std::string temp_length;
+        std::string temp_text;
+        for (int i = position + 2; i < position + 3; ++i) {
+            temp_length.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        int temp_length_i = std::stoi(temp_length, nullptr, 2);
+        for (int i = position + 3; i < position + 3 + temp_length_i; ++i) {
+            temp_text.append(std::bitset<8>(this -> data_[i]).to_string());
+        }
+        this -> cue_point_ = BinToString(&temp_text);
+        this -> event_check_[6] = 1;
+    }
+}
+void Track::ReadTrackData() {
+
+    for (int i = 0; i < this->length_; ++i) {
+        this->temp_data.append(std::bitset<8>(this->data_[i]).to_string());
+    }
+    this -> SetText();
+    this -> SetCopyright();
+    this -> SetTrackName();
+    this -> SetInstrumentName();
+    this -> SetLyric();
+    this -> SetMarker();
+    this -> SetCuePoint();
 }
